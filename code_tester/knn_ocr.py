52bc8fab1aa5390d7b7f81a7177eb89a33ea49ab
Ye Binbin <phantom2501@gmail.com>
2015-09-12 12:38:52 +0900

    knn snippet ok

--- code_tester/knn_ocr.py
+++ code_tester/knn_ocr.py
@@ -6,7 +6,19 @@ import numpy as np
 
 trainData = np.load('../training_data/imageTrain.npy')
 trainLabel = np.load('../training_data/labelTrain.npy')
-trainData = np.array(trainData, np.float32)
-trainLabel = np.array(trainLabel, np.float32)
+
+trainData = trainData.reshape(60000, 784).astype(np.float32)
+trainLabel = trainLabel.reshape(-1).astype(np.float32)
+
 knn = cv2.KNearest()
 knn.train(trainData, trainLabel)
+
+testData = np.load('../training_data/imageTest.npy')
+testData = testData.reshape(10000, 784).astype(np.float32)
+test = testData[0].reshape(1,784)
+
+retval, results, neighborResponses, dists = knn.find_nearest(test, k = 5)
+print retval, results, neighborResponses, dists
+
+testLabel = np.load('../training_data/labelTest.npy')
+print testLabel[0]
