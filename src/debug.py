3922a9dee2af177dd802ae3bda4744e2f9f68d84
Ye Binbin <phantom2501@gmail.com>
2015-10-20 02:01:21 +0900

    update debug

--- src/debug.py
+++ src/debug.py
@@ -1,6 +1,9 @@
 __author__ = 'braineo'
 
 import sudoku_parser
-
+import sudoku_solver
 sudoku = sudoku_parser.SudokuParser()
-sudoku.parse("../test_picture/sudoku1.jpg")
+solver = sudoku_solver.SudokuSolver()
+puzzle = sudoku.parse("../test_picture/sudoku1.jpg")
+solution = solver.solve(puzzle)
+print solution
