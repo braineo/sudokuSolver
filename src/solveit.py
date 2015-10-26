03d340e21b66df2673f50be79cd0302599905f5f
Ye Binbin <phantom2501@gmail.com>
2015-10-27 00:03:36 +0900

    update readme

--- src/solveit.py
+++ src/solveit.py
@@ -1,16 +1,13 @@
 import sudoku_parser
 import sudoku_solver
 from cv2 import imshow, waitKey
-from sys import
-from os import listdir
-from os.path import isfile, join
-test_path = "../test_picture/"
-# sudoku_puzzles = [join(test_path, f) for f in listdir(test_path) if isfile(join(test_path, f))]
+from sys import argv
+
 sudoku = sudoku_parser.SudokuParser()
 solver = sudoku_solver.SudokuSolver()
-#for puzzle in sudoku_puzzles:
-    #print puzzle
-puzzle = sudoku.parse("../test_picture/sudoku9.jpg")
+if not argv[1]:
+    raise IndexError
+puzzle = sudoku.parse(argv[1])
 solution = solver.solve(puzzle)
 sudoku.draw_solution(solution)
 imshow('result', sudoku.resized_largest_square)
